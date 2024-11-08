deb6b83c8c554348f7f560299b3009052e63e968
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2024-11-08 16:07:47 +0000

    bug check

--- eval.py
+++ eval.py
@@ -292,7 +292,7 @@ def eval_faiss(emb_dir,
         max_len = int(max_test_seq_len[ti])
         assert type(max_len) == int, f'{max_len} is not int. See {max_test_seq_len}'
 
-        assert type(test_seq_len) == np.ndarray
+        assert type(test_seq_len) == np.ndarray, f'{max_test_seq_len.dtype} is not np.ndarray'
         test_seq_len = test_seq_len[test_seq_len <= max_len]
 
         for si, sl in enumerate(test_seq_len):
