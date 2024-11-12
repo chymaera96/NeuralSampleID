fec7a90e6925a8ead69c238cd1713cb9c0ca64bf
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2024-11-12 16:55:01 +0000

    logs

--- eval.py
+++ eval.py
@@ -354,9 +354,9 @@ def eval_faiss(emb_dir,
     # Summary 
     valid_mask = (test_seq_len <= max_test_seq_len[:, None])        # The mask preserves valid entries
     print("Valid mask: ", valid_mask)
-    top1_rate = 100. * np.mean(np.where(valid_mask, top1_exact, np.nan), axis=0)
-    top3_rate = 100. * np.mean(np.where(valid_mask, top3_exact, np.nan), axis=0)
-    top10_rate = 100. * np.mean(np.where(valid_mask, top10_exact, np.nan), axis=0)
+    top1_rate = 100. * np.nanmean(np.where(valid_mask, top1_exact, np.nan), axis=0)
+    top3_rate = 100. * np.nanmean(np.where(valid_mask, top3_exact, np.nan), axis=0)
+    top10_rate = 100. * np.nanmean(np.where(valid_mask, top10_exact, np.nan), axis=0)
 
     hit_rates = np.stack([top1_rate, top3_rate, top10_rate], axis=0)
     # del fake_recon_index, query, db
