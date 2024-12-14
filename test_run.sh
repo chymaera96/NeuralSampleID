b20a50d597baf5ddd4d7820cab8943ac6aef0a29
Aditya Bhattacharjee <acw723@sbg9.apocrita>
2024-12-14 20:09:57 +0000

    test_run.sh

--- test_run.sh
+++ test_run.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
+!/bin/bash
 #$ -cwd
 #$ -j y
-#$ -pe smp 8          # cores per GPU
-#$ -l h_rt=36:0:0    # hours runtime
-#$ -l h_vmem=11G      #  RAM per core
-#$ -l gpu=1           # request GPU
+#$ -pe smp 24          # 16 cores per GPU
+#$ -l h_rt=1:0:0    # 40 hours runtime
+#$ -l h_vmem=7.5G      # 11G RAM per core
+#$ -l gpu=2           # request 2 GPU
 #$ -l gpu_type=ampere
-#$ -N grafp_tc29_medeval
-#$ -o /data/home/acw723/GraFP/hpc_out
+#$ -N nsid_tc_0_eval
+#$ -o /data/home/acw723/NeuralSampleID/hpc_out
 #$ -m beas
 
 module load python/3.10.7
 source ../grafp_venv/bin/activate
 python test_fp.py --query_lens=1,2,3,5 \
                   --text=sample100_ivfpq \
-                  --test_dir=../dataset/sample_100/audio
+                  --test_dir=../datasets/sample_100/audio
