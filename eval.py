bug check: candidate vec overflow issue

eval.py
@@ -330,7 +330,9 @@ def eval_faiss(emb_dir,
                 candidate_seq = fake_recon_index[cid:(cid + sl), :] 
                 print(f'q.shape: {q.shape}, candidate_seq.shape: {candidate_seq.shape}')
                 if candidate_seq.shape[0] < sl:
-                    q_match = q[:candidate_seq.shape[0], :]      
+                    q_match = q[:candidate_seq.shape[0], :] 
+                else:
+                    q_match = q     
                 score = np.mean(np.sum(q_match * candidate_seq, axis=1))
                 hist[match] += score
                 # To-do: use cosine distance for better matching score