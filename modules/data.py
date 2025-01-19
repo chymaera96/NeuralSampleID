4e4853f1a81c258acbb773eb5c7121c19c019a6d
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2025-01-19 14:28:01 +0000

    stem-based dataset class

--- modules/data.py
+++ modules/data.py
@@ -5,15 +5,12 @@ from torch.utils.data import Dataset
 import torch.nn.functional as F
 import torchaudio
 import numpy as np
-import librosa
-import torch.nn as nn
 import warnings
-import pandas as pd
+from itertools import compress
 
-from util import load_index, get_frames, qtile_normalize, qtile_norm
+from util import load_index, qtile_norm
 
-
-class NeuralfpDataset(Dataset):
+class NeuralSampleIDDataset(Dataset):
     def __init__(self, cfg, path, transform=None, train=False):
         self.path = path
         self.transform = transform
@@ -34,58 +31,76 @@ class NeuralfpDataset(Dataset):
         print(f"Loaded {len(self.filenames)} files from {path}")
         self.ignore_idx = []
         self.error_counts = {}
-        
+
     def __getitem__(self, idx):
         if idx in self.ignore_idx:
             return self[idx + 1]
-        
-        datapath = self.filenames[str(idx)]
-        try:
-            # with warnings.catch_warnings():
-            #     warnings.simplefilter("ignore")
-            audio, sr = torchaudio.load(datapath)
 
+        datapath = self.filenames[idx]  # List of filepaths per sample
+        try:
+            audio_dict = {stem: torchaudio.load(path)[0].mean(dim=0) for stem, path in datapath.items()}
+            sr = torchaudio.load(datapath['mix'])[1]
         except Exception:
-            print("Error loading:" + self.filenames[str(idx)])
+            print("Error loading:", datapath)
             self.error_counts[idx] = self.error_counts.get(idx, 0) + 1
             if self.error_counts[idx] > self.error_threshold:
                 self.ignore_idx.append(idx)
-            return self[idx+1]
-
-        audio_mono = audio.mean(dim=0)
-        
-        resampler = torchaudio.transforms.Resample(sr, self.sample_rate)
-        audio_resampled = resampler(audio_mono)    
-
-        clip_frames = int(self.sample_rate*self.dur)
-        
-        if len(audio_resampled) <= clip_frames:
-            # self.ignore_idx.append(idx)
             return self[idx + 1]
-    
-        
-        #   For training pipeline, output a random frame of the audio
-        if self.train:
-            a_i = audio_resampled
-            a_j = a_i.clone()
-            
-            offset_mod = int(self.sample_rate*(self.offset) + clip_frames)
-            if len(audio_resampled) < offset_mod:
-                print(len(audio_resampled), offset_mod)
-            r = np.random.randint(0,len(audio_resampled)-offset_mod)
-            ri = np.random.randint(0,offset_mod - clip_frames)
-            rj = np.random.randint(0,offset_mod - clip_frames)
-            clip_i = a_i[r:r+offset_mod]
-            clip_j = a_j[r:r+offset_mod]
-            x_i = clip_i[ri:ri+clip_frames]
-            x_j = clip_j[rj:rj+clip_frames]
 
-            if x_i.abs().max() < self.silence or x_j.abs().max() < self.silence:
-                print("Silence detected. Skipping...")
+        resampler = torchaudio.transforms.Resample(sr, self.sample_rate)
+        audio_dict = {stem: resampler(audio) for stem, audio in audio_dict.items()}
+
+        clip_frames = int(self.sample_rate * self.dur)
+
+        if len(audio_dict['mix']) <= clip_frames:
+            return self[idx + 1]
+
+        # For training pipeline, output random frame of the audio
+        if self.train:
+            a_i = audio_dict['mix']
+            stem_keys = list(audio_dict.keys())
+            stem_keys.remove('mix')
+
+            # Randomly sample stems to include in x_j
+            selected_stems = np.random.choice([True, False], size=len(stem_keys))
+            selected_keys = list(compress(stem_keys, selected_stems))
+
+            if not selected_keys:
                 return self[idx + 1]
-            
+
+            a_j = torch.stack([audio_dict[stem] for stem in selected_keys], dim=0)
+
+            offset_mod = int(self.sample_rate * (self.offset) + clip_frames)
+
+            if len(a_i) < offset_mod or any([len(channel) < offset_mod for channel in a_j]):
+                return self[idx + 1]
+
+            r = np.random.randint(0, len(a_i) - offset_mod)
+            ri = np.random.randint(0, offset_mod - clip_frames)
+
+            clip_i = a_i[r:r + offset_mod]
+            clip_j = a_j[:, r:r + offset_mod]
+
+            x_i = clip_i[ri:ri + clip_frames]
+            x_j = clip_j[:, ri:ri + clip_frames]
+
+            # Silence detection using SNR
+            signal_power = torch.mean(x_i**2)
+            valid_channels = []
+
+            for channel in x_j:
+                noise_power = torch.mean((channel - x_i)**2)
+                snr = 10 * torch.log10(signal_power / (noise_power + 1e-8))
+                if snr >= -20:
+                    valid_channels.append(channel)
+
+            if not valid_channels:
+                return self[idx + 1]
+
+            x_j = torch.stack(valid_channels, dim=0)
+
             if self.norm is not None:
-                norm_val = qtile_norm(audio_resampled, q=self.norm)
+                norm_val = qtile_norm(a_i, q=self.norm)
                 x_i = x_i / norm_val
                 x_j = x_j / norm_val
 
@@ -101,21 +116,20 @@ class NeuralfpDataset(Dataset):
             else:
                 x_i = x_i[:clip_frames]
 
-            if len(x_j) < clip_frames:
-                x_j = F.pad(x_j, (0, clip_frames - len(x_j)))
-            else:    
-                x_j = x_j[:clip_frames]
-
+            if x_j.shape[1] < clip_frames:
+                x_j = F.pad(x_j, (0, clip_frames - x_j.shape[1]), mode='constant', value=0)
+            else:
+                x_j = x_j[:, :clip_frames]
 
             return x_i, x_j
-        
-        #   For validation / test, output consecutive (overlapping) frames
+
+        # For validation / test, output consecutive (overlapping) frames
         else:
-            # Not implemented yet; raise error
             raise NotImplementedError("Validation pipeline not implemented yet")
-    
+
     def __len__(self):
         return len(self.filenames)
+
     
 
 class Sample100Dataset(Dataset):
