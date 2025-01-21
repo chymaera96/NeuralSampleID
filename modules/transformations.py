b98ccc2bb28433b1ecb50fd8b475ae8850b49c66
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2025-01-21 12:32:40 +0000

    w/o bandeq

--- modules/transformations.py
+++ modules/transformations.py
@@ -36,7 +36,7 @@ class GPUTransformSampleID(nn.Module):
         ]
 
         self.train_transform_j_options = [
-            BandEQ(),
+            # BandEQ(),
             # BitCrush(min_bit_depth=cfg['min_bit_depth'], max_bit_depth=cfg['min_bit_depth'], p=1.0),
             Gain(min_gain_db=-cfg['gain'], max_gain_db=cfg['gain'], p=1.0),
             ApplyImpulseResponse(ir_path=self.ir_dir, p=1.0),
