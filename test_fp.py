0d8dec4bfec6eeda760d91a7391d55b92cea9ee0
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2025-01-27 06:58:46 +0000

    tc_17 full eval: epoch 175

--- test_fp.py
+++ test_fp.py
@@ -285,11 +285,11 @@ def main():
             else:
                 print("=> Skipping dummy db creation...")
 
-            # create_ref_db(ref_db_loader, augment=test_augment,
-            #                 model=model, output_root_dir=fp_dir, verbose=True)
+            create_ref_db(ref_db_loader, augment=test_augment,
+                            model=model, output_root_dir=fp_dir, verbose=True)
             
-            # create_query_db(query_db_loader, augment=test_augment,
-            #                 model=model, output_root_dir=fp_dir, verbose=True)
+            create_query_db(query_db_loader, augment=test_augment,
+                            model=model, output_root_dir=fp_dir, verbose=True)
             
             
             text = f'{args.text}_{str(epoch)}'
