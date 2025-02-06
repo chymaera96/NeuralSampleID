4ba53fbe68c8a129d7ba67d7a9b74ef7e9979e2f
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2025-02-06 20:01:05 +0000

    bug check

--- encoder/resnet_ibn.py
+++ encoder/resnet_ibn.py
@@ -9,7 +9,7 @@ class IBN(nn.Module):
         half1 = int(channels * 0.5)
         self.IN = nn.InstanceNorm2d(half1, affine=True)
         self.BN = nn.BatchNorm2d(channels - half1)
-    
+
     def forward(self, x):
         split = torch.split(x, [x.shape[1] // 2, x.shape[1] - x.shape[1] // 2], dim=1)
         out = torch.cat([self.IN(split[0]), self.BN(split[1])], dim=1)
@@ -26,19 +26,19 @@ class ResidualIBN(nn.Module):
         self.conv3 = nn.Conv2d(out_channels, out_channels, kernel_size=1, stride=1, bias=False)
         self.bn3 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
-        
+
         self.downsample = None
         if stride != 1 or in_channels != out_channels:
             self.downsample = nn.Sequential(
                 nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(out_channels)
             )
-    
+
     def forward(self, x):
         identity = x
         if self.downsample is not None:
             identity = self.downsample(x)
-        
+
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
@@ -46,7 +46,7 @@ class ResidualIBN(nn.Module):
         out = self.bn2(out)
         out = self.conv3(out)
         out = self.bn3(out)
-        
+
         out += identity
         out = self.relu(out)
         return out
@@ -62,19 +62,19 @@ class ResidualBlock(nn.Module):
         self.conv3 = nn.Conv2d(out_channels, out_channels, kernel_size=1, stride=1, bias=False)
         self.bn3 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
-        
+
         self.downsample = None
         if stride != 1 or in_channels != out_channels:
             self.downsample = nn.Sequential(
                 nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(out_channels)
             )
-    
+
     def forward(self, x):
         identity = x
         if self.downsample is not None:
             identity = self.downsample(x)
-        
+
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
@@ -82,7 +82,7 @@ class ResidualBlock(nn.Module):
         out = self.bn2(out)
         out = self.conv3(out)
         out = self.bn3(out)
-        
+
         out += identity
         out = self.relu(out)
         return out
@@ -93,7 +93,7 @@ class GeMPooling(nn.Module):
         super(GeMPooling, self).__init__()
         self.p = nn.Parameter(torch.ones(1) * p)
         self.eps = eps
-    
+
     def forward(self, x):
         return F.adaptive_avg_pool2d(x.clamp(min=self.eps).pow(self.p), (1, 1)).pow(1.0 / self.p)
 
@@ -104,30 +104,31 @@ class ResNetIBN(nn.Module):
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        
+
         self.layer1 = self._make_layer(ResidualIBN, 64, 256, 3, stride=1)
         self.layer2 = self._make_layer(ResidualIBN, 256, 512, 4, stride=1)
         self.layer3 = self._make_layer(ResidualIBN, 512, 1024, 6, stride=2)
         self.layer4 = self._make_layer(ResidualBlock, 1024, 2048, 3, stride=2)
-        
+
         self.global_pool = GeMPooling()
-        
-    
+
+
     def _make_layer(self, block, in_channels, out_channels, blocks, stride):
         layers = []
         layers.append(block(in_channels, out_channels, stride))
         for _ in range(1, blocks):
             layers.append(block(out_channels, out_channels))
         return nn.Sequential(*layers)
-    
+
     def forward(self, x):
+        print(f"Input: {x.shape}")
         x = self.conv1(x)
         # print(f"Conv1: {x.shape}")
         x = self.bn1(x)
         x = self.relu(x)
         x = self.maxpool(x)
         # print(f"Maxpool: {x.shape}")
-        
+
         x = self.layer1(x)
         # print(f"Layer1: {x.shape}")
         x = self.layer2(x)
@@ -136,8 +137,8 @@ class ResNetIBN(nn.Module):
         # print(f"Layer3: {x.shape}")
         x = self.layer4(x)
         # print(f"Layer4: {x.shape}")
-        
+
         x = self.global_pool(x).view(x.size(0), -1)
-        # print(f"Global Pool: {x.shape}")
-        x = torch.flatten(x, 1)
+        print(f"Global Pool: {x.shape}")
         return x
+
