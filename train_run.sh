d3f735d7bf29a69485d5b7cdd757e3ee16dbb22f
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2025-02-07 10:05:34 +0000

    merge conflict resolution

--- train_run.sh
+++ train_run.sh
@@ -2,14 +2,18 @@
 #$ -cwd
 #$ -j y
 #$ -pe smp 16          # 16 cores per GPU
-#$ -l h_rt=40:0:0    # 40 hours runtime
+#$ -l h_rt=20:0:0    # 40 hours runtime
 #$ -l h_vmem=11G      # 11G RAM per core
 #$ -l gpu=2           # request 2 GPU
 #$ -l gpu_type=ampere
-#$ -N grafp_tc30
-#$ -o /data/home/acw723/GraFP/hpc_out
+#$ -N nsid_tc_16
+#$ -o /data/home/acw723/NeuralSampleID/hpc_out
 #$ -m beas
 
 module load python/3.10.7
 source ../grafp_venv/bin/activate
-python train.py --ckp=tc_30
+module load gcc/12.1.0
+python train.py --ckp=tc_16
+
+
+
