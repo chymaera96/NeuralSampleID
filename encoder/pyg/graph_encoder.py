c01b041ef908b1807a1f938f8a47070f14ca6f04
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2025-02-21 18:35:22 +0000

    bug check

--- encoder/pyg/graph_encoder.py
+++ encoder/pyg/graph_encoder.py
@@ -1,6 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from encoder.pyg.gcn_lib import Block
 
 class Downsample(nn.Module):
