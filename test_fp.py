44716b05d51c24eb43fe5f75ff3ab11bdb2a603e
chymaera96 <aditya.bhattacharjee01@estudiant.upf.edu>
2025-02-26 06:36:05 +0000

    no recomputation quick match

--- test_fp.py
+++ test_fp.py
@@ -303,15 +303,15 @@ def main():
             else:
                 print("=> Skipping dummy db creation...")
 
-            create_ref_db(ref_db_loader, augment=test_augment,
-                            model=model, output_root_dir=fp_dir, verbose=True)
+            # create_ref_db(ref_db_loader, augment=test_augment,
+            #                 model=model, output_root_dir=fp_dir, verbose=True)
             
             # create_query_db(query_db_loader, augment=test_augment,
             #                 model=model, output_root_dir=fp_dir, verbose=True)
             
-            if args.map:
-                create_query_db(query_full_db_loader, augment=test_augment,
-                                model=model, output_root_dir=fp_dir, fname='query_full_db', verbose=True)
+            # if args.map:
+            #     create_query_db(query_full_db_loader, augment=test_augment,
+            #                     model=model, output_root_dir=fp_dir, fname='query_full_db', verbose=True)
             
             
             text = f'{args.text}_{str(epoch)}'
